99930e1e668a5f6db35ca3a09df21c22a4be6222
M-D-Y <muratovd@yandex.ru>
2016-07-23 14:08:53 +0300

    1

--- ch04-javascript/src/com/corejsf/EmptyBean.java
+++ ch04-javascript/src/com/corejsf/EmptyBean.java
@@ -1,26 +1,22 @@
-
+package com.corejsf;
 
 import java.io.Serializable;
 
-//import javax.faces.bean.ManagedBean; 
-    import javax.inject.Named;
-//import javax.faces.bean.SessionScoped; 
-    import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
 
 //@ManagedBean(name="user") // 
 @Named("emptyBean")
 @SessionScoped
 public class EmptyBean implements Serializable {
-   private String name = "EmptyBean";
+/*	private String name = "EmptyBean";
 
-public String getName() {
-	return name;
-}
+	public String getName() {
+		return name;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
-
-
-
+	public void setName(String name) {
+		this.name = name;
+	}
+*/
 }
